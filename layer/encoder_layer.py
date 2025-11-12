0e135115a2efbe153f00c8cb0af3da252db325a7
Nakamura Sota <so.dev000@gmail.com>
2025-11-12 22:47:39 +0900

    add encoder layer

--- layer/encoder_layer.py
+++ layer/encoder_layer.py
@@ -1,9 +1,34 @@
+import torch
 from torch import nn
+
+from component.feed_forward import FeedForward
+from component.multihead_attention import MultiheadAttention
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, model_dim):
         super().__init__()
+        self.attention = MultiheadAttention(model_dim)
+        self.normalizer_1 = nn.LayerNorm(model_dim)
+        self.feed_forward = FeedForward(model_dim)
+        self.normalizer_2 = nn.LayerNorm(model_dim)
 
     def forward(self, inputs):
-        pass
+        context = self.attention(inputs)
+        normalized_1 = self.normalizer_1(inputs + context)
+        feed_forward_out = self.feed_forward(normalized_1)
+        normalized_2 = self.normalizer_2(normalized_1 + feed_forward_out)
+        return normalized_2
+
+
+if __name__ == "__main__":
+    batch_size = 3
+    seq_len = 6
+    model_dim = 512
+
+    input = torch.randn(batch_size, seq_len, model_dim)
+    encoder = EncoderLayer(model_dim=model_dim)
+
+    print(f"Input shape: {input.shape}")  # (3, 6, 512)
+    output = encoder.forward(input)
+    print(f"Output shape: {output.shape}")  # (3, 6, 512)
