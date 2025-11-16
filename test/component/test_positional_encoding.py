d1087917ca4d607040cb20301f1c627b5e7df9f5
Nakamura Sota <so.dev000@gmail.com>
2025-11-16 14:37:59 +0900

    add positional encoding

---
+++ test/component/test_positional_encoding.py
@@ -0,0 +1,16 @@
+import torch
+
+from component.positional_encoding import PositionalEncoding
+
+
+class TestPositionalEncoding:
+    def test_output_shape(self):
+        model_dim = 512
+        batch_size = 2
+        seq_len = 10
+
+        pe = PositionalEncoding(model_dim=model_dim, dropout=0.0)
+        inputs = torch.randn(batch_size, seq_len, model_dim)
+        output = pe(inputs)
+
+        assert output.shape == (batch_size, seq_len, model_dim)
